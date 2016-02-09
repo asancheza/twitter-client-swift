76833d61445fe48d8e56df095f679a2217a40365
eMobc SL <emobc@eMobcs-MacBook-Pro.local>
2016-02-08 22:01:19 -0800

    Creating repository and first files

---
+++ Twitter Client/TwitterClient.swift
@@ -0,0 +1,26 @@
+//
+//  TwitterClient.swift
+//  Twitter Client
+//
+//  Created by eMobc SL on 08/02/16.
+//  Copyright © 2016 Neurowork. All rights reserved.
+//
+
+import Cocoa
+
+let twitterConsumerKey = ""
+let twitterConsumerSecret = ""
+let twitterBaseUrl = NSURL(string: "https://api.twitter.com")
+
+class TwitterClient: BDBOAuth1SessionManager {
+    class var sharedInstance: TwitterClient {
+        struct Static {
+            static let instance = TwitterClient(baseURL: twitterBaseUrl,
+                consumerKey: twitterConsumerKey,
+                consumerSecret: twitterConsumerSecret)
+        }
+        
+        return Static.instance
+    }
+
+}
